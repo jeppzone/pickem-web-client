ddf87795d83fce1ecac4a51ebfabc3dde4b61396
Jesper Olsson Laine <jespols@student.chalmers.se>
2023-01-19 13:31:23 +0100

    Split statistics into regular and post season

--- app/src/Statistics.svelte
+++ app/src/Statistics.svelte
@@ -5,6 +5,8 @@
   import LoadingIndicator from "./LoadingIndicator.svelte";
 
   let statistics = [];
+  let regularSeasonStatistics = [];
+  let postSeasonStatistics = [];
   let seasons = [2022, 2021];
   let season = 2022;
   let loading = false;
@@ -12,12 +14,16 @@
   onMount(async () => {
     loading = true;
     statistics = await getStatistics($loggedInUser, season);
+    regularSeasonStatistics = statistics?.filter((s) => s.seasonType === "Reg");
+    postSeasonStatistics = statistics?.filter((s) => s.seasonType === "Post");
     loading = false;
   });
 
   async function fetchStatistics() {
     loading = true;
     statistics = await getStatistics($loggedInUser, season);
+    regularSeasonStatistics = statistics?.filter((s) => s.seasonType === "Reg");
+    postSeasonStatistics = statistics?.filter((s) => s.seasonType === "Post");
     loading = false;
   }
 </script>
@@ -35,8 +41,20 @@
     {:else if statistics?.length === 0}
       <h3>{`No statistics yet for ${season}`}</h3>
     {:else}
+      <h2>Regular season</h2>
       <table>
-        {#each statistics as statEntry}
+        {#each regularSeasonStatistics as statEntry}
+          <tr>
+            <td>{statEntry.description}</td>
+            <td>{statEntry.value}</td>
+            <td>{statEntry.user}</td>
+            <td>{statEntry.valueDescription}</td>
+          </tr>
+        {/each}
+      </table>
+      <h2>Post season</h2>
+      <table>
+        {#each postSeasonStatistics as statEntry}
           <tr>
             <td>{statEntry.description}</td>
             <td>{statEntry.value}</td>
@@ -69,6 +87,10 @@
   }
   tr:nth-child(even) {
     background-color: rgb(12, 35, 49);
+  }
+
+  h2 {
+    text-align: center;
   }
 
   @media only screen and (max-width: 800px) {
